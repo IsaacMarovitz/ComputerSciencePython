458f86bd49f3500f78ac527e547ebea26944cbf9
Isaac Marovitz <42140194+IsaacMarovitz@users.noreply.github.com>
2020-09-09 16:14:23 +0100

    Update Conversation.py

--- Conversation.py
+++ Conversation.py
@@ -1,9 +1,10 @@
+# Python Homework 09/09/2020
+
 print("\nHey there!", end=" ")
 user_name = input("What's your name? ")
 print("Nice to meet you " + user_name)
 user_age = int(input("How old are you? (Please only input numbers) "))
 print("So, you were born in " + str((2020-user_age)) + "!")
-print("Nice")
 user_colour = input("What's your favourite colour? ")
 if user_colour.lower() == "blue":
     print("Hey, my favourite is blue too!")
