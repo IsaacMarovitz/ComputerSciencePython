3a463ba2b0a01c64fb7187bdccd2f221ef453c30
Isaac Marovitz <42140194+IsaacMarovitz@users.noreply.github.com>
2020-09-09 16:12:26 +0100

    Create Conversation.py

---
+++ Conversation.py
@@ -0,0 +1,12 @@
+print("\nHey there!", end=" ")
+user_name = input("What's your name? ")
+print("Nice to meet you " + user_name)
+user_age = int(input("How old are you? (Please only input numbers) "))
+print("So, you were born in " + str((2020-user_age)) + "!")
+print("Nice")
+user_colour = input("What's your favourite colour? ")
+if user_colour.lower() == "blue":
+    print("Hey, my favourite is blue too!")
+else:
+    print(user_colour + "'s a pretty colour! But my favourite it blue")
+print("Goodbye! 👋")
