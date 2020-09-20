31623c2ac8235243337bf37f8bebde6bb50d5dad
Isaac Marovitz <42140194+IsaacMarovitz@users.noreply.github.com>
2020-09-20 17:15:14 +0100

    Regex

--- Choose Your Own Adventure/Adventure.py
+++ Choose Your Own Adventure/Adventure.py
@@ -13,7 +13,6 @@ import sys, json, os, time, threading, re
 
 charWrapLimit = 100
 currentRoomIndex = 0
-playerName = ""
 quick = False
 
 try:
@@ -41,6 +40,7 @@ def typewriter(message):
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(0.02)
+        # Source End
         charCount = charCount + 1
         if char == "\r":
             charCount = 0
@@ -104,19 +104,20 @@ storyData = json.loads(storyData)
 def go(inputMessage):
     global currentRoomIndex
     global quick
-    inputMessage = inputMessage.title()
-    if inputMessage in storyData['rooms'][currentRoomIndex]['roomConnections']:
+    originalMessage = inputMessage.strip().title()
+    inputMessage = inputMessage.lower().replace(" ", "")
+    if inputMessage in str(storyData['rooms'][currentRoomIndex]['roomConnections']).lower().replace(" ", ""):
         for room in storyData['rooms']:
-            if room['roomName'] == inputMessage:
+            if str(room['roomName']).lower().replace(" ", "") == inputMessage:
                 currentRoomIndex = storyData['rooms'].index(room)
-                typewriter(f"You move into {inputMessage}.\n")
+                typewriter(f"You move into {originalMessage}.\n")
                 quick = False
                 waitForEnter()
                 inputReceivedEvent.wait()
                 getCommand()
                 return True
     else:
-        typewriter(f"There's no connecting room called {inputMessage}.\n")
+        typewriter(f"There's no connecting room called {originalMessage}.\n")
         waitForEnter()
         inputReceivedEvent.wait()
         getCommand()
@@ -126,7 +127,29 @@ def look(inputMessage):
     global currentRoomIndex
     if inputMessage == "around":
         try:
-            typewriter(f"You look around. {storyData['rooms'][currentRoomIndex]['roomLookAround']}\n")
+            roomLookAround = f"You look around. {str(storyData['rooms'][currentRoomIndex]['roomLookAround'])}\n"
+            if "{" in roomLookAround:
+                try:
+                    placeHolders = re.findall(r"{(.+), (.+), (.+), (.+)}", roomLookAround)
+                    completeSection = re.findall(r"{.+}", roomLookAround)[0]
+                    for info in placeHolders:
+                        try:
+                            index = int(info[0])
+                            if bool(storyData['rooms'][currentRoomIndex]['roomInteractables'][index][info[3]]):
+                                roomLookAround = roomLookAround.replace(completeSection, info[2])
+                            else:
+                                roomLookAround = roomLookAround.replace(completeSection, info[1])
+                        except ValueError:
+                            playerValue = str(info[0])
+                            if bool(storyData['player'][0][playerValue]):
+                                roomLookAround = roomLookAround.replace(completeSection, info[2])
+                            else:
+                                roomLookAround = roomLookAround.replace(completeSection, info[1])
+                except IndexError:
+                    completeSection = re.findall(r"{.+}", roomLookAround)[0]
+                    for info in placeHolders:
+                        roomLookAround = roomLookAround.replace(completeSection, info[1])
+                typewriter(roomLookAround)
         except KeyError:
             typewriter("Hmmmm. I'm missing some data about this room. Your story.json file may be incomplete.\n")
         waitForEnter()
@@ -134,42 +157,106 @@ def look(inputMessage):
         getCommand()
         return True
     else:
-        inputMessage = inputMessage.title()
+        originalMessage = inputMessage.strip().title()
+        inputMessage = inputMessage.lower().replace(" ", "")
         for interactable in storyData['rooms'][currentRoomIndex]['roomInteractables']:
-            if interactable['interactableName'] == inputMessage:
+            if str(interactable['interactableName']).lower().replace(" ", "") == inputMessage:
                 typewriter(interactable['interactableText'] + "\n")
                 waitForEnter()
                 inputReceivedEvent.wait()
                 getCommand()
                 return True
-        typewriter(f"No interactables called {inputMessage}.\n")
+        typewriter(f"No interactables called {originalMessage}.\n")
         waitForEnter()
         inputReceivedEvent.wait()
         getCommand()
         return False
 
+def useCheck(useable):
+    if not useable['interactableUseableDisabled']:
+        if useable['interactableUseableState']:
+            userBool = getBooleanUserInput(str(useable['interactableUseText1']), "Please only input y/n.\n")
+            inputReceivedEvent.wait()
+            if userBool:
+                useable['interactableUseableState'] = False
+                typewriter(f"{useable['interactableName']} was set to false.\n")
+                waitForEnter()
+                inputReceivedEvent.wait()
+                getCommand()
+                return True
+            else:
+                userBool = getBooleanUserInput(str(useable['interactableUseText0']), "Please only input y/n.\n")
+                inputReceivedEvent.wait()
+                if userBool:
+                    useable['interactableUseableState'] = True
+                    typewriter(f"{useable['interactableName']} was set to true.\n")
+                    waitForEnter()
+                    inputReceivedEvent.wait()
+                    getCommand()
+                    return True
+    else:
+        typewriter(f"{useable['interactableDisabled']}\n")
+        waitForEnter()
+        inputReceivedEvent.wait()
+        getCommand()
+        return True
+
 def use(inputMessage):
     global currentRoomIndex
-    inputMessage = inputMessage.title()
+    originalMessage = inputMessage.strip().title()
+    inputMessage = inputMessage.lower().replace(" ", "")
     for useable in storyData['rooms'][currentRoomIndex]['roomInteractables']:
-        if useable['interactableName'] == inputMessage and useable['interactableUseable']:
-            if not useable['interactableUseableDisabled']:
-                if useable['interactableUseableState']:
-                    userBool = getBooleanUserInput(str(useable['interactableUseText1']), "Please only input y/n.\n")
-                    inputReceivedEvent.wait()
-                    if userBool:
-                        useable['interactableUseableState'] = False
-                        typewriter(f"{useable['interactableName']} was set to false.\n")
-                else:
-                    userBool = getBooleanUserInput(str(useable['interactableUseText0']), "Please only input y/n.\n")
-                    inputReceivedEvent.wait()
-                    if userBool:
-                        useable['interactableUseableState'] = True
-                        typewriter(f"{useable['interactableName']} was set to true.\n")
+        if str(useable['interactableName']).lower().replace(" ", "") == inputMessage and useable['interactableUseable']:
+            if not useable['interactableCollectable']:
+                useCheck(useable)
             else:
-                typewriter(f"{useable['interactableDisabled']}\n")
-        else:
-            typewriter(f"No usable objects called {inputMessage}.\n")
+                for useable in storyData['inventory']:
+                    if str(useable['interactableName']).lower().replace(" ", "") == inputMessage:
+                        if useable['interactableSingleUse'] == True:
+                            userBool = getBooleanUserInput(str(useable['interactableUseText']), "Please only input y/n.\n")
+                            inputReceivedEvent.wait()
+                            if userBool:
+                                storyData['player'][0][useable['interactableUseStat']] + useable['interactableUseStrength']
+                                typewriter(f"Used {originalMessage}. {str(useable['interactableUseStat']).title()} increased by {str(useable['interactableUseStrength'])}.\n")
+                                storyData['inventory'].remove(useable)
+                        else:
+                            useCheck(useable)
+                typewriter(f"Add {originalMessage} to your inventory to use it.\n")
+                waitForEnter()
+                inputReceivedEvent.wait()
+                getCommand()
+                return False
+                    
+    typewriter(f"No usable objects called {originalMessage}.\n")
+    waitForEnter()
+    inputReceivedEvent.wait()
+    getCommand()
+    return False
+
+def take(inputMessage):
+    global currentRoomIndex
+    originalMessage = inputMessage.strip().title()
+    inputMessage = inputMessage.lower().replace(" ", "")
+    for collectable in storyData['rooms'][currentRoomIndex]['roomInteractables']:
+        if str(collectable['interactableName']).lower().replace(" ", "") == inputMessage and collectable['interactableCollectable']:
+            storyData['inventory'].append(collectable)
+            storyData['rooms'][currentRoomIndex]['roomInteractables'].remove(collectable)
+            typewriter(f"{originalMessage} was added to your inventory.\n")
+            waitForEnter()
+            inputReceivedEvent.wait()
+            getCommand()
+            return True
+
+    typewriter(f"No collectable objects called {originalMessage}.\n")
+    waitForEnter()
+    inputReceivedEvent.wait()
+    getCommand()
+    return False
+
+def stats():
+    typewriter("You stats are as follows.\n")
+    typewriter(f"   Player Name - {storyData['player'][0]['name']}\n")
+    typewriter(f"   HP - {str(storyData['player'][0]['health'])}\n")
     waitForEnter()
     inputReceivedEvent.wait()
     getCommand()
@@ -187,18 +274,29 @@ def helpCmd(inputMessage):
         typewriter("look ___ = This command will give you more information about interactable objects in the current room. Type 'look around' to get a summary of interactable objects in the current room.\n")
     elif inputMessage == "use":
         typewriter("use ___ = This command will use the object you pass in.\n")
+    elif inputMessage == "take":
+        typewriter("take ___ = This comamnd will take the object you pass in and add it to your inventory.\n")
     elif inputMessage == "inventory":
         typewriter("inventory = This command will print the current items in your inventory.\n")
+    elif inputMessage == "stats":
+        typewriter("stats = This command will tell you your current player stats.\n")
     elif inputMessage == "help":
         typewriter("help ___ = This command will give you more information about a given command, or will tell you about all available commands.\n")
     else:
-        typewriter('''Avilable Commands:\n    go\n    look\n    use\n    inventory\n    help\nType help followed by each command to learn more information about them.\n''')
+        typewriter('''Avilable Commands:\n    go\n    look\n    use\n    take\n    inventory\n    stats\n    help\nType help followed by each command to learn more information about them.\n''')
     waitForEnter()
     inputReceivedEvent.wait()
     getCommand()
 
 def inventory(inputMessage):
-    typewriter("There's no inventory system yet lol.\n")
+    typewriter("You have the following items in your inventory.\n")
+    for collectable in storyData['inventory']:
+        try:
+            typewriter(f"   {collectable['interactableName']} - {collectable['interactableInventoryDescription']}\n")
+        except KeyError:
+            typewriter("Hmmmm. I'm missing some data about this item. Your story.json file may be incomplete.\n")
+    waitForEnter()
+    inputReceivedEvent.wait()
     getCommand()
 
 def parseUserInput(inputMessage):
@@ -228,6 +326,16 @@ def parseUserInput(inputMessage):
             waitForEnter()
             inputReceivedEvent.wait()
             getCommand()
+    elif textArray[0] == "take":
+        try:
+            take(inputMessage.replace('take', '').strip())
+        except IndexError:
+            typewriter("No object to take given.\n")
+            waitForEnter()
+            inputReceivedEvent.wait()
+            getCommand()
+    elif textArray[0] == "stats":
+        stats()
     elif textArray[0] == "help":
         try:
             helpCmd(textArray[1])
@@ -268,15 +376,31 @@ def displayRoom():
     global quick
     try:
         roomName = str(storyData['rooms'][currentRoomIndex]['roomName'])
-        roomDescription = str(storyData['rooms'][currentRoomIndex]['roomDescription']) + "\n"
+        roomDescription = str(storyData['rooms'][currentRoomIndex]['roomDescription']) + "\n\n"
         # Solution taken from Source 4
-        # This finds any instance of {} I put in my roomDescription text, and replaces it with some regex magic.
-        placeHolders = re.findall(r"{(\w+)}", roomDescription)
-        for index in placeHolders:
-            if bool(storyData['rooms'][currentRoomIndex]['roomInteractables'][int(index)]['interactableUseableState']):
-                roomDescription = roomDescription.replace('{'+index+'}', 'on')
-            else:
-                roomDescription = roomDescription.replace('{'+index+'}', 'off')
+        # This finds any instance of {} I put in my roomDescription text, and turns it into an array of values using regex magic. It took me so long to find a regex value that worked
+        # and I don't know why it does but it's probably fine.
+        if "{" in roomDescription:
+            try:
+                placeHolders = re.findall(r"{(.+), (.+), (.+), (.+)}", roomDescription)
+                completeSection = re.findall(r"{.+}", roomDescription)[0]
+                for info in placeHolders:
+                    try:
+                        index = int(info[0])
+                        if bool(storyData['rooms'][currentRoomIndex]['roomInteractables'][index][info[3]]):
+                            roomDescription = roomDescription.replace(completeSection, info[2])
+                        else:
+                            roomDescription = roomDescription.replace(completeSection, info[1])
+                    except ValueError:
+                        playerValue = str(info[0])
+                        if bool(storyData['player'][0][playerValue]):
+                            roomDescription = roomDescription.replace(completeSection, info[2])
+                        else:
+                            roomDescription = roomDescription.replace(completeSection, info[1])
+            except IndexError:
+                completeSection = re.findall(r"{.+}", roomDescription)[0]
+                for info in placeHolders:
+                    roomDescription = roomDescription.replace(completeSection, info[1])
         printHeader(roomName)
         if not quick:
             typewriter(roomDescription)
@@ -290,10 +414,8 @@ def displayRoom():
     
 
 # TO DO:
-# ADD TAKE COMMAND
 # ADD ASCII ART
 # ADD UI
-# IMPLEMENT INVENTORY
 
 # Program
 clear()
@@ -316,9 +438,9 @@ if not skipIntro:
     clear()
 
     printHeader("Setup")
-    playerName = getTextUserInput("What's your name?: ", "Please only input strings.\n")
+    storyData['player'][0]['name'] = getTextUserInput("What's your name?: ", "Please only input strings.\n")
     inputReceivedEvent.wait()
-    typewriter(f"Nice to meet you {playerName}.\n")
+    typewriter(f"Nice to meet you {storyData['player'][0]['name']}.\n")
     typewriter("Let's go on an adventure!\n")
     typewriter("If you ever feel stuck or unsure use the 'help' command to get a summary of each usable command.\n")
     typewriter("Please bare in mind that each command will only work with basic statments like 'look computer' not 'look at the computer'.\n")
